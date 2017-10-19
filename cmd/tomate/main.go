b51b178b9097d19a053c144ed3ed14a9f9868cf7
matbesancon <mathieu.besancon@gmail.com>
2017-10-19 14:18:59 -0400

    parse options from CLI

--- cmd/tomate/main.go
+++ cmd/tomate/main.go
@@ -1,11 +1,18 @@
- package main
+package main
 
 import (
-  "os"
-  "github.com/mbesancon/tomate"
+	"flag"
+	"os"
+
+	"github.com/mbesancon/tomate"
 )
 
- func main() {
-   tmt := tomate.New(20, 5, 7, 3)
-   tmt.Loop(os.Stdout)
- }
+func main() {
+	sprintLength := flag.Int("sprint", 30, "Duration of the sprints")
+	pauseLength := flag.Int("spause", 5, "Duration of the short pauses")
+	longPauseLength := flag.Int("lpause", 10, "Duration of the long pauses")
+	nsprints := flag.Int("nsprints", 4, "Number of sprints between long pauses")
+	flag.Parse()
+	tmt := tomate.New(*sprintLength, *pauseLength, *longPauseLength, *nsprints)
+	tmt.Loop(os.Stdout)
+}
